517e775313f08e9e4e7901125a24993184891680
rohit kumar <kumarrohit9337@gmail.com>
2025-02-17 00:28:06 +0530

    Done

--- Websocket-backend/src/index.ts
+++ Websocket-backend/src/index.ts
@@ -1,10 +1,31 @@
 import { request } from 'express';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer , WebSocket } from 'ws';
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 
 const JWT_SECRET = process.env.JWT_SECRET ?? "121212"; // Secret key for JWT
 const wss = new WebSocketServer({ port: 8080 });
+
+// Here we will make a global state to store the user and the no of rooms the user has joined and rhe msg he has sent
+// Also interface for the user
+
+interface User{
+  ws: WebSocket;
+  userId: string;
+  rooms: string[];
+}
+
+const users: User[] = [];
+
+// this function will check the user is authenticated or not to join the socket connection
+function checkUserAuth(token: string): JwtPayload | null {
+  const decode = jwt.verify(token , JWT_SECRET)
+  if (!decode || !(decode as JwtPayload).userId) {
+    return null;
+  }
+  return decode as JwtPayload;
+
+} 
 
 wss.on('connection', function connection(ws) {
   // basically in this first it wiil checkk the authoriuxation and then give  the 
@@ -15,16 +36,58 @@ wss.on('connection', function connection(ws) {
   }
   const queryParam = new URLSearchParams(url.split('?')['1']);
   const token = queryParam.get("token") || "";
- // now we have to decod this token and the check the user
- const decode = jwt.verify(token , JWT_SECRET)
- if (!decode || !(decode as JwtPayload).userId) {
-  ws.close();
-  return;
-  
- }
+ // now we have to decod this token and the check the user is authenticated or not
+  const userAuthenticated = checkUserAuth(token);
+
+  if (userAuthenticated == null){
+    ws.send("You are not authenticated");
+    ws.close();
+    return null;
+  }
+
+// now push the data to the user array
+users.push({
+  userId: userAuthenticated.userId,
+  ws,
+  rooms: []
+})
+
 
   ws.on('message', function message(data) {
-    ws.send('Hello rohit');
+    // parssse the data 
+    const parseData = JSON.parse(data as unknown as string);  // {type: join_room , roomId: 132}
+
+    if(parseData.type === 'join_room'){
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parseData.roomId);
+    }
+
+    // if i want to remove that room 
+    if (parseData.type === 'leave_room'){
+      const user = users.find(x => x.ws === ws);
+      if(!user){
+        return;
+
+      }
+      user.rooms = user.rooms.filter(x => x === parseData.roomId);
+    }
+
+    // if i want to send the message to the room
+    if (parseData.type === 'chat') {
+      const roomId = parseData.roomId;
+      const message = parseData.message;
+
+      users.forEach(user => {
+        if (user.rooms.includes(roomId)) {
+          user.ws.send(JSON.stringify({
+            type: 'chat',
+            message: message,
+            roomId
+          }))
+        }
+      })
+      
+    }
   });
 
   
