f4625fd1911676d330399e82f9caeeb566932906
rohit kumar <kumarrohit9337@gmail.com>
2025-02-18 17:51:39 +0530

    Manage the room id

--- fronted2/src/app/canvas/[roomid]/page.tsx
+++ fronted2/src/app/canvas/[roomid]/page.tsx
@@ -1,72 +1,19 @@
-'use client';
 
-import React, { useEffect, useRef } from 'react'
+import Canvas from '@/components/Canvas';
+export default async function canvasPage({params}: {
+  params: {
+    roomId: string;
+  };
+}) {
 
-export default function canvas() {
-    // for using canvas we have to take the ref of the canvas using the userRef hook
-    const Canvasref = useRef<HTMLCanvasElement>(null);
-
-
-    // useeffect to change the context while the component is mounted using the ref
-
-    useEffect(() => {
-
-        if (Canvasref.current) {
-            const canvas =  Canvasref.current;
-            const ctx  = canvas.getContext('2d'); 
-
-            if (!ctx) {
-                return;
-            }
-
-            // when element get cliecked then it will move
-
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
+  const roomId = await params.roomId;
+  console.log(roomId);
 
 
-             // now take the mouse event to draw on the canvas and the move it 
-
-            canvas.addEventListener("mousedown" , (event) => {
-                clicked = true;
-                startX = event.clientX;
-                startY = event.clientY;
-             });
-
-            canvas.addEventListener("mouseup" , (event) => {
-                clicked = false;
-                console.log(event.clientX);
-                console.log(event.clientY)
-             });
-
-             canvas.addEventListener("mousemove" , (event) => {
-                if (clicked) {
-                    const height = event.clientY - startY;
-                    const width = event.clientX - startX;
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.strokeRect(startX, startY, width, height);
-                    // console.log(event.clientX);
-                    // console.log(event.clientY)
-                    
-                }
-             });
-
-             
-
-
-            ctx.strokeRect(10, 10, 100, 100);
-
-        }
-
-    } , [Canvasref])
+  return <Canvas roomId={roomId}/>
 
    
     
-  return (
-    <div>
-      <canvas height={1500} width={1500}  ref={Canvasref} className='bg-white'></canvas>
-    </div>
-  )
+ 
 }
 
---
+++ fronted2/src/drawElement/index.ts
@@ -0,0 +1,123 @@
+
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
+
+
+
+// interface of tjhe shape
+type Shape =  {
+    type: 'rect',
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+} | {
+    type: 'circle',
+    centreX: number;
+    centerY: number;
+    radius: number;
+} 
+
+export async function  drawintial(canvas: HTMLCanvasElement , roomId: string) {
+    const ctx  = canvas.getContext('2d'); 
+
+            if (!ctx) {
+                return;
+            }
+
+            ctx.fillStyle = 'black';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            // take all the shape in the array
+            let existingShapes: Shape[] =await getExistingShapes(roomId);
+
+            // when element get cliecked then it will move
+            clearContext(canvas, ctx, existingShapes);
+            let clicked = false;
+            let startX = 0;
+            let startY = 0;
+
+
+             // now take the mouse event to draw on the canvas and the move it 
+
+            canvas.addEventListener("mousedown" , (event) => {
+                const rect = canvas.getBoundingClientRect(); // Get canvas position
+                clicked = true;
+                startX = event.clientX;
+                startY = event.clientY;
+             });
+
+            canvas.addEventListener("mouseup" , (event) => {
+                clicked = false;
+                // get the existing shape
+                const height = event.clientY - startY;
+                const width = event.clientX - startX;
+                existingShapes.push({
+                    type: 'rect',
+                    x: startX,
+                    y: startY,
+                    width,
+                    height
+                });
+             });
+
+             canvas.addEventListener("mousemove" , (event) => {
+                if (clicked) {
+                    const height = event.clientY - startY;
+                    const width = event.clientX - startX;
+                    clearContext(canvas, ctx, existingShapes);
+                    ctx.strokeStyle = 'white';
+                    ctx.strokeRect(startX, startY, width, height);
+                    // console.log(event.clientX);
+                    // console.log(event.clientY)
+                }
+             });
+
+            ctx.strokeRect(10, 10, 100, 100);
+
+        }
+
+        // fxn for the clear the cntext and the shape 
+
+
+        function clearContext(canvas: HTMLCanvasElement , ctx: CanvasRenderingContext2D , existingShapes: Shape[]) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = 'black';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            // use map for the exting shape 
+            existingShapes.map((shape) => {
+                if (shape.type === 'rect') {
+                    ctx.strokeStyle = 'white';
+                    ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+                    ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
+                    
+                }
+            })
+        }
+
+
+
+        // fteching the data from the backend i.e the existing shape
+
+export async function getExistingShapes(roomId: string) {
+    try {
+        const response = await axios.get(`${BACKEND_URL}/chats/${roomId}`);
+        const data = response.data.messages;
+
+        // Map over the data and return the shapes
+        const shapes = messages.map((x : {message: string}) => {
+            const messageData = JSON.parse(x.message);
+            return messageData;
+
+        });
+
+        return shapes;
+        
+    } catch (error) {
+        console.log('Error fetching existing shapes:', error);
+        return null;
+    }
+}
+
+
